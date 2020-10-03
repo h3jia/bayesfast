a54e39d25a177112437a5109b5d9ec7a11fe4bd5
HerculesJack <he.jia.phy@gmail.com>
2020-10-02 20:27:32 -0700

    get local version # from git

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import numpy as np
 from extension_helpers import add_openmp_flags_if_available
-import warnings
+import warnings, os, subprocess
 
 
 ext_modules = [
@@ -39,9 +39,40 @@ if not all(openmp_added):
                   RuntimeWarning)
 
 
+# from numpy/setup.py, which is distributed under BSD 3-Clause
+# https://github.com/numpy/numpy/blob/master/setup.py
+# https://github.com/numpy/numpy/blob/master/LICENSE.txt
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH', 'HOME']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.check_output(cmd, stderr=subprocess.STDOUT, env=env)
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except (subprocess.SubprocessError, OSError):
+        GIT_REVISION = "Unknown"
+
+    if not GIT_REVISION:
+        # this shouldn't happen but apparently can (see gh-8512)
+        GIT_REVISION = "Unknown"
+
+    return GIT_REVISION
+
+
 setup(
     name='bayesfast',
-    version='0.1.0.dev2',
+    version='0.1.0.dev2+' + git_version()[:7],
     author='He Jia and Uros Seljak',
     maintainer='He Jia',
     maintainer_email='he.jia.phy@gmail.com',
